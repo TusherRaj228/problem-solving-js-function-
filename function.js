c682c0993e583d8bebbedc37b5eb3bbd2628aa81
TusherRaj228 <tusherahmmedraj@gmail.com>
2025-08-11 21:18:34 +0600

    function problem solving

---
+++ function.js
@@ -0,0 +1,16 @@
+const numbers = 45;
+
+function test(){
+    console.log('1st function test');
+    console.log('2nd test');
+
+
+}
+test();
+
+function add_Numbers( a, b, c, d, e){
+    const total = a+b+c+d+e;
+    console.log(total);
+}
+add_Numbers(5,7,9,2,3);
+add_Numbers(2,4,6,8);
